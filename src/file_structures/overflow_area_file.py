83b075b05f50687e1007b4f08bbd7a6161aed3de
Marek.Hering <marek.hering99@gmail.com>
2020-12-10 10:15:20 +0100

    add inheritance to other file class

---
+++ src/file_structures/overflow_area_file.py
@@ -0,0 +1,6 @@
+from .file_class import FileClass
+
+
+class OverflowAreaFile(FileClass):
+    def __init__(self, file_directory):
+        super(OverflowAreaFile).__init__(file_directory)
---
+++ src/file_structures/primary_area_file.py
@@ -0,0 +1,6 @@
+from .file_class import FileClass
+
+
+class PrimaryAreaFile(FileClass):
+    def __init__(self, file_directory):
+        super(PrimaryAreaFile).__init__(file_directory)

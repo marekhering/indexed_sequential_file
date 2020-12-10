6fe38c381d84d239dad98b4948daf4296c50962c
Marek.Hering <marek.hering99@gmail.com>
2020-12-10 10:29:56 +0100

    add init function to AppCore

--- src/app_core.py
+++ src/app_core.py
@@ -1,5 +1,10 @@
 from .file_structures import *
+from setup import INDEXES_DIR, PRIMARY_AREA_DIR, OVERFLOW_AREA_DIR
+
 
 class AppCore:
     def __init__(self):
-        pass
+        self.index_file = IndexFile(INDEXES_DIR)
+        self.primary_area = PrimaryAreaFile(PRIMARY_AREA_DIR)
+        self.overflow_area = OverflowAreaFile(OVERFLOW_AREA_DIR)
+

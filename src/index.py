312d618de45c8e88269da4bf91098f0687c749d3
Marek.Hering <marek.hering99@gmail.com>
2020-12-10 09:49:39 +0100

    add get methods for index

--- src/index.py
+++ src/index.py
@@ -1,5 +1,11 @@
 
 class Index:
-    def __init__(self, key, page_number):
+    def __init__(self, key, page_offset):
         self.key = key
-        self.page_number = page_number
+        self.page_offset = page_offset
+
+    def get_key(self):
+        return self.key
+
+    def get_page_offset(self):
+        return self.page_offset
